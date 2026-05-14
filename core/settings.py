f7b3550481022b29e81ef8b3645cdb0e3c384b2a
Omar Ahmed Ayad <132781252+omar3yad@users.noreply.github.com>
2026-05-14 16:55:40 +0300

    e

--- core/settings.py
+++ core/settings.py
@@ -69,11 +69,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 # ===== DATABASE (single — reads from .env) =====
+# امسح الكود القديم وحط ده مؤقتاً
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse('postgresql://postgres:CnArXtQLrTUGCTcUQdYicJbddyBIgulg@switchyard.proxy.rlwy.net:59049/railway')
 }
 
 # ===== PASSWORD VALIDATION =====
